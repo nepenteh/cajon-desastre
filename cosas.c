59b8d58992e0baff1ec2f2a004d9b87b9c8eea50
José Manuel Rosado <109131721+nepenteh@users.noreply.github.com>
2025-04-12 18:33:14 +0200

    Update cosas.c
    Añado presentación

--- cosas.c
+++ cosas.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void presentacion() {
+  printf("Programa principal");
+}
+
 int main() {
   int cosas = 10;
   int x;
 
+  presentacion();
   printf("Introduce un número x: ");
   scanf("%d",&x);
   cosas += x; // comentario añadido en local
