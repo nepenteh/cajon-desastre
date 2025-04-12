ed09aac5eedc5317f4d0a70f88ac05dfb4a10433
Jose Manuel Rosado <nepenteh@gmail.com>
2025-04-12 17:38:22 +0200

    liberia de graficos

---
+++ graficos/lib_gra.c
@@ -0,0 +1,16 @@
+void presentacion() {
+    printf("\n\n");
+    printf("  __  __    _    __  __ _____ \n");
+    printf(" |  \\/  |  / \\  |  \\/  | ____|\n");
+    printf(" | |\\/| | / _ \\ | |\\/| |  _|  \n");
+    printf(" | |  | |/ ___ \\| |  | | |___ \n");
+    printf(" |_|  |_/_/   \\_\\_|  |_|_____|\n");
+    printf("\n\n");
+}
+
+void linea(int n) {
+    for (int i = 0; i < n; i++) {
+        printf("-");
+    }
+    printf("\n");
+}
